bb07957bef09bfde05b0dd6745b5b4ba5b2cd3fc
Diego F Garcia <gadifer54@gamil>
2024-02-19 23:11:41 -0500

    testimonios-Mi-App

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimonio () {
+    return(
+        <div className="contenedor-testimonio">
+            <img className="imagen-testimonio"
+                 src={require("../imagenes/Testimonio-Maria.png")}
+                 alt="Foto de Maria"/>
+
+        </div>
+
+    );
+}
