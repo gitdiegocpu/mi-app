c2471523b07b6454f1e32ef7596e6d39b9c1b4aa
Diego F Garcia <gadifer54@gamil>
2024-02-20 23:10:32 -0500

    primer-testimonio-maria

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Testimonio from './componentes/Testimonio.jsx';
 
 function App() {
   return (
     <div className="App">
-      <>hola casa</>
+      <Testimonio />
     </div>
   );
 }
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import FotoMaria from '../Imagenes/Testimonio-Maria.png';
 
 function Testimonio () {
     return(
         <div className="contenedor-testimonio">
             <img className="imagen-testimonio"
-                 src={require("../imagenes/Testimonio-Maria.png")}
+                 src={FotoMaria}
                  alt="Foto de Maria"/>
+                 <div className="contenedor-texto-textimonio">
+                    <p className="nombre-testimonio"> Maria Bostian en Suecia </p>
+                    <p className="cargo-testimonio"> Ingenieria de Software en Spotify </p>
+                    <p className="texto-testimonio"> La ingeniria en sistemas es mi pasion, lo encontre en el 6to grado
+                                                     desde entonces soy un amante a todo lo que sea computarizado, estoy totalmente
+                                                     agradecida con la vida por todo lo que hago.
+                                                     y si puedo ayudarte en algo, solo avisame aqui estare.</p>
+
+                 </div>
 
         </div>
 
     );
 }
+
+export default Testimonio;
